8e42ed5e8af71cf522e6306bb2288f8496f27d09
calabrodavide <davidegiovanni03@gmail.com>
2024-06-13 14:45:16 +0200

    better trimming

--- other/tools.py
+++ other/tools.py
@@ -11,18 +11,17 @@ def read_pdf(path):
     
 # trims the table removing all None values and cells with len < 2 and remove the useless "Numero ore" and luogo column
 def trimPDF(table):
+    res = []
     try:
         for row in table:
+            row = [cell if cell is not None else '' for cell in row]
             if row [6] != '':
                 row[5] = row[6]
             row.pop(7)
             row.pop(6)
             row.pop(4)
-            for i, cell in enumerate(row):
-                if cell is None:
-                    row.pop(i)
-                elif len(cell) < 2:
-                    row.pop(i)
-        return table 
+            row = [cell for cell in row if len(cell) > 2]
+            res.append(row)
+        return res 
     except Exception:
         return []
